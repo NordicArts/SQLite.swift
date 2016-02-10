bb1063f1307a7c2dad3164eb96e33fd7c9947f5e
Keloran <keloran@nordicarts.net>
2016-02-10 21:28:14 +0000

    Fix

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
   name: "SwiftSQLite",
   dependencies: [
-    .Package(url: "https://github.com/NordicArts/SQLite.wrapper", majorVersion: 1)
+    .Package(url: "https://github.com/NordicArts/SQLite.module", majorVersion: 1)
   ]
 )
